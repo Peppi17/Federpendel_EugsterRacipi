a39e7f3e00107d13a63bfa867a380052f3a12d61
Peppi17 <75023934+Peppi17@users.noreply.github.com>
2020-12-13 22:17:12 +0100

    Update button.py

--- Federpendel_Eugster_Racipi_v2/button.py
+++ Federpendel_Eugster_Racipi_v2/button.py
@@ -9,5 +9,5 @@ def button(kontur_dicke, kontur_farbe, button_farbe_r, button_farbe_g, button_fa
     rect(button_xpos, button_ypos, button_laenge, button_breite)
     textAlign(CENTER)
     fill(255)
-    textSize(20)
+    textSize(25)
     text(name, button_xpos + button_laenge/2 , button_ypos + 2*button_breite/3)
