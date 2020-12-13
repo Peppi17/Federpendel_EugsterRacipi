71334c40f4248d672b92e4b27f5828bc5d103b54
Peppi17 <75023934+Peppi17@users.noreply.github.com>
2020-12-13 21:17:23 +0100

    Create button.py

---
+++ Federpendel_Eugster_Racipi_v2/button.py
@@ -0,0 +1,13 @@
+
+
+
+def button(kontur_dicke, kontur_farbe, button_farbe_r, button_farbe_g, button_farbe_b , button_xpos, button_ypos, button_laenge, button_breite, name):
+    
+    strokeWeight(kontur_dicke)
+    stroke(kontur_farbe, kontur_farbe, 0)
+    fill(button_farbe_r, button_farbe_g, button_farbe_b)
+    rect(button_xpos, button_ypos, button_laenge, button_breite)
+    textAlign(CENTER)
+    fill(255)
+    textSize(20)
+    text(name, button_xpos + button_laenge/2 , button_ypos + 2*button_breite/3)
