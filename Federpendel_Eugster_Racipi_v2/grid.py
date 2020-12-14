0693d0390ff3ec2730633402ca98c9f6e9924f67
fracipi <75132220+fracipi@users.noreply.github.com>
2020-12-14 12:29:16 +0100

    Update grid.py

--- Federpendel_Eugster_Racipi_v2/grid.py
+++ Federpendel_Eugster_Racipi_v2/grid.py
@@ -1,27 +1,35 @@
-
-
+# Erstellt das Raster
 
 def grid(xscl, yscl, xmax, xmin, ymax, ymin, text_size):
-    background(255) # weisser Hintergrund
+    
+    # weisser Hintergrund
+    background(255) 
 
-    for i in range(0, (xmax-xmin)/xscl): # Senkrechte Linien
+    # Senkrechte Linien
+    for i in range(0, (xmax-xmin)/xscl):
         strokeWeight(1)
         stroke(220)
         line(i*xscl, ymax, i*xscl, ymin)
 
-    for i in range(0, (ymax-ymin)/yscl): # Waagrechte Linien
+    # Waagrechte Linien
+    for i in range(0, (ymax-ymin)/yscl): 
         strokeWeight(1)
         stroke(220)
         line(xmin, i*yscl, xmax, i*yscl) # Zwei Linien gespiegelt an der X-Achse
         line(xmin, -i*yscl, xmax, -i*yscl)
     
+    # Achsen
     strokeWeight(2)
     stroke(0)
     line(xmin, 0, xmax, 0) # X-Achse
     line(xmin, ymin, xmin, ymax) # Y-Achse
+    
+    # Dreieckspitzen der Achsen
     fill(0)
     triangle(xmax,0,xmax-5,5,xmax-5,-5) # Spitze X-Achse
     triangle(0,ymin,-5,ymin+5,5,ymin+5) # Spitze  Y-Achse
+    
+    # Beschriftungen der Achsen
     textSize(text_size)
     textAlign(LEFT, TOP)
     text("Amplitude A", 20,ymin+20) # Text "Amplitude A" zur Y-Achse
