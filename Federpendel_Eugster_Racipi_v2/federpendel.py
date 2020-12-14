c7416b777f2d07e52497e352c4b3fc937cc0b0a5
Peppi17 <75023934+Peppi17@users.noreply.github.com>
2020-12-14 16:36:10 +0100

    Update federpendel.py

--- Federpendel_Eugster_Racipi_v2/federpendel.py
+++ Federpendel_Eugster_Racipi_v2/federpendel.py
@@ -1,17 +1,17 @@
 # Zeichnet die Animation (Balken oben, "Feder" als Linie und anhaengende/r Ball/Masse)
 
-def federpendel(A, omega, t, streckung, objX, objY, obj_laenge, obj_breite, factor_k, factor_m): 
+def federpendel(A, omega, t, streckungY, objX, objY, obj_laenge, obj_breite, factor_k, factor_m): 
     
    
     # Linie ("Feder" vereinfacht dargestellt als Linie)
     strokeWeight(2+factor_k/PI)
     stroke(100)
-    line(objX, objY + obj_breite, objX,-A*cos(omega*t)*streckung) # Linie("Feder") und Balken oben, an dem der Ball angemacht ist
+    line(objX, objY + obj_breite, objX,-A*cos(omega*t)*streckungY) # Linie("Feder") und Balken oben, an dem der Ball angemacht ist
     
     # Ball
     strokeWeight(5+factor_m*20)
     stroke(255, 100, 0)
-    point(objX, -A*cos(omega*t)*streckung) # Kreieren der neuen Zeichnung (Ball und Linie)
+    point(objX, -A*cos(omega*t)*streckungY) # Kreieren der neuen Zeichnung (Ball und Linie)
  
     #Balken
     noStroke()
