16c6b6b04c7f19ec7a09cab4169ea3fca2dc3c1f
fracipi <75132220+fracipi@users.noreply.github.com>
2020-12-14 13:10:18 +0100

    Update timer.py

--- Federpendel_Eugster_Racipi_v2/timer.py
+++ Federpendel_Eugster_Racipi_v2/timer.py
@@ -3,6 +3,6 @@
 def timer(posX, posY, t, text_size):
     fill(0)
     textAlign(LEFT, TOP)
-    text("Zeit:  ", posX-10*text_size, posY+20)
+    text("Zeit:  ", posX-10*text_size, posY+15)
     textAlign(RIGHT, TOP)
-    text(str(int(t)) + " Sekunden", posX-20, posY+20)
+    text(str(int(t)) + " Sekunden", posX-20, posY+15)
