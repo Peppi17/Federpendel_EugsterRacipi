56e3598ecff198edb824f3118e055b00f8598d6c
Peppi17 <75023934+Peppi17@users.noreply.github.com>
2020-12-14 07:57:54 +0100

    Update grid.py

--- Federpendel_Eugster_Racipi_v2/grid.py
+++ Federpendel_Eugster_Racipi_v2/grid.py
@@ -23,6 +23,8 @@ def grid(xscl, yscl, xmax, xmin, ymax, ymin, text_size):
     triangle(xmax,0,xmax-5,5,xmax-5,-5) # Spitze X-Achse
     triangle(0,ymin,-5,ymin+5,5,ymin+5) # Spitze  Y-Achse
     textSize(text_size)
-    textAlign(LEFT)
-    text("Zeit t",xmax-80, 30) # Text "Zeit t" zur X-Achse
-    text("Amplitude A",15,ymin+30) # Text "Amplitude A" zur Y-Achse
+    textAlign(LEFT, TOP)
+    text("Amplitude A", 20,ymin+20) # Text "Amplitude A" zur Y-Achse
+    textAlign(RIGHT, TOP)
+    text("Zeit t",xmax-20, 20) # Text "Zeit t" zur X-Achse
+    
