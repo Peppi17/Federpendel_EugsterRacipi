3f0032b97847f5eae905d8d83aa5619256739d31
Peppi17 <75023934+Peppi17@users.noreply.github.com>
2020-12-15 22:27:01 +0100

    Update button.py

--- Federpendel_Eugster_Racipi_v2/button.py
+++ Federpendel_Eugster_Racipi_v2/button.py
@@ -1,16 +1,20 @@
 # Erstellt einen Knopf abhaengig von Position, Laenge, Breite, Farbe, Konturfarbe, Konturdicke, Beschriftung und Textgroesse
 
-def button(kontur_dicke, kontur_farbe, button_farbe_r, button_farbe_g, button_farbe_b , button_xpos, button_ypos, button_laenge, button_breite, name, text_size):
+def button(kontur_dicke, kontur_farbe, button_farbe_r, button_farbe_g, button_farbe_b , button_xpos, button_ypos, button_laenge, button_breite, name, text_groesse):
     
+    # Schatten vom Knopf
     noStroke()
     fill(50)
     rect(button_xpos+4, button_ypos+4, button_laenge, button_breite)
-
+    
+    # Aussehen Knopf und Text
     strokeWeight(kontur_dicke)
     stroke(kontur_farbe, kontur_farbe, 0)
-    fill(button_farbe_r, button_farbe_g, button_farbe_b)
+    fill(button_farbe_r, button_farbe_g, button_farbe_b) #RGB Farbcode
     rect(button_xpos, button_ypos, button_laenge, button_breite)
     textAlign(CENTER)
     fill(255)
-    textSize(text_size)
+    textSize(text_groesse)
     text(name, button_xpos + button_laenge/2 , button_ypos + 2*button_breite/3)
+    
+    
